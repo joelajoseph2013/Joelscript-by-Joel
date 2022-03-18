a504f229b45bb5c51fbe88d11b5ea27e3b0b0f25
joelajoseph2013 <99537694+joelajoseph2013@users.noreply.github.com>
2022-03-18 14:07:04 +0530

    Add files via upload
    Actually this is a web thats useless.... So there is nothing pretty much to do....

---
+++ main.js
@@ -0,0 +1,326 @@
+function enter() {
+    inputtext = document.getElementById("inputtext").value;
+    if (inputtext == "GetCredits") {
+        document.getElementById("result").innerHTML = "Html and Java were used coded by Swastik modified by Joel and made advanced version ;";
+    }
+    else if (inputtext == "CtrlPoint") {
+        document.getElementById("result").innerHTML = "Success added +20 points Ctrlpoint unlocked";
+    }
+    else if (inputtext == "install usb virus") {
+        document.getElementById("result").innerHTML = "01010101010101010101001010101010101010110101001010101010101010101010010101010101010101010101001010100101010010101010101010101010010101010101010101101010010101010101010101010100101010101010101010101010010101001010100101010101010101010100101010101010101011010100101010101010101010101001010101010101010101010100101010010101001010101010101010101001010101010101010110101001010101010101010101010010101010101010101010101001010100101010";
+    }
+    else if (inputtext == "remove usb virus") {
+        document.getElementById("result").innerHTML = "Removed USB156723";
+    }
+    else if (inputtext == "site.reload") {
+       document.getElementById("result").innerHTML = "Reloading...";
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+    }
+    else if (inputtext == "ver.exe") {
+        document.getElementById("result").innerHTML = "Version: 1.1 Joelscript";
+    }
+    else if (inputtext == "script-AllowHacker") {
+        document.getElementById("result").innerHTML = "Alert! Hacker Allowed. Type 62_091ReH to remove hacker";
+    }
+    else if (inputtext == "62_091ReH") {
+        document.getElementById("result").innerHTML = "Hacker01 successfully removed";
+    }
+    else if (inputtext == "install memz") {
+        document.getElementById("result").innerHTML = "An malicious virus is added please type MaliMemzDel";
+    }
+    else if (inputtext == "MaliMemzDel") {
+        document.getElementById("result").innerHTML = "successfully removed a virus";
+    }
+    else if (inputtext == "Directory") {
+        document.getElementById("result").innerHTML = "C:/Users/Joel/Desktop/Joelscript.exe";
+    }
+    else if (inputtext == "Scripts.txt") {
+        document.getElementById("result").innerHTML = "Html and Java were used coded by Swastik modified by Joel and made advanced version ";
+    }
+    else if (inputtext == "files.txt") {
+        document.getElementById("result").innerHTML = "Joelscript.html,main.js No CSS";
+    }
+    else if (inputtext == "Ram.txt") {
+        document.getElementById("result").innerHTML = "4Bytes";
+    }
+    else if (inputtext == "GreenFileBackround.txt") {
+        document.getElementById("result").innerHTML = "backroundcolor-BgColor-Green";
+    }
+    else if (inputtext == "12sG23sD23fGoN324tsmDLsn45sogaofohnsooneahsk12.gib") {
+        document.getElementById("result").innerHTML = "YOU ARE NOT SUPPOSED TO ENTER THIS CODE YOU DONT HAVE PERMISSION";
+    }
+    else if (inputtext == "-Hack line - mode GET ADMINADVANCED 12sG23sD23fGoN324tsmDLsn45sogaofohnsooneahsk12.gib") {
+        document.getElementById("result").innerHTML = "main.exe DO NOT DELETE";
+    }
+    else if (inputtext == "-Hack line - mode delete file 12sG23sD23fGoN324tsmDLsn45sogaofohnsooneahsk12.gib") {
+       document.getElementById("result").innerHTML = "Reloading Script...";
+       
+       location.reload();
+    }
+    else if (inputtext == "-Hack line - mode site.reload INF") {
+       document.getElementById("result").innerHTML = "Reloading Script...";
+       
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+       location.reload();
+
+    }
+
+}
